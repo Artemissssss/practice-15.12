02caf37e5f03dd1aa17da34365bcd56a6a164fa1
Artemissssss <64923345+Artemissssss@users.noreply.github.com>
2022-12-26 14:22:17 +0200

    Add files via upload

--- src/components/ModalImg/ModalImg.js
+++ src/components/ModalImg/ModalImg.js
@@ -1,19 +1,52 @@
-import {useState} from 'react'
+import {useState , useEffect} from 'react'
 import style from './ModalImg.module.css'
-
+let secondState = true;
 function ModalImg({ img }) {
+    console.log("HELLO!")
     const [onHider, setOnHider] = useState(true);
+    setOnHider(true)
+    const [SecondState, setSecondState] = useState(true);
+    const [pon, setPon] = useState(true);
+    useEffect(() => {
+        if(secondState){
+            setOnHider(true)
+        }else{
+
+            setPon(!pon)
+            if(secondState){
+                setPon(!pon)
+                setSecondState(true)
+            }else{
+                setPon(!pon)
+                secondState=true;
+            }
+            console.log(pon)
+        }
+        console.log(SecondState)
+    }, [pon]);
     const Hider = () =>{
         setOnHider(false);
+        setSecondState(false)
+        secondState = false;
     }
-    if(onHider){
-  return (
+    return(
+        <>
+        {onHider ? (
     <div onClick={Hider} className={style.ModalImgSect}>
-        <img src={img} alt="model of car scaled"  className={style.ModalImg}/>
+        <img onClick={(event) =>{event.stopPropagation()}} src={img} alt="model of car scaled"  className={style.ModalImg}/>
     </div>
+    ) : (<div>ok</div>)}
+        </>
     )
-    }else{
-        return null;
-    }
+// if(onHider){
+//   return (
+//     <div onClick={Hider} className={style.ModalImgSect}>
+//         <img onClick={(event) =>{event.stopPropagation()}} src={img} alt="model of car scaled"  className={style.ModalImg}/>
+//     </div>
+//     )
+//     }else{
+//         secondState=true;
+//         return null;
+//     }
 }
 export default ModalImg
